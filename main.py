b765312bab4d8771df1521f1e0f8e4404a45e9e1
enanram <dm_92@live.co.uk>
2021-05-19 17:15:04 +0100

    created simple matcher

--- main.py
+++ main.py
@@ -1,34 +1,89 @@
+################################################################
+######################### SECRET SANTA #########################
+################################################################
+# A script that matches members of a group to each other while #
+# avoiding matching pairs who have matched in the past.        #
+################################################################
+###################### by Donagh Marnane #######################
+
+
+
 from participant import Participant
 import random
 import csv
 
-def importCsvFile():
+'''
+A new session is initiated when a .csv file is opened. This will check
+for users who have exceptions. If no exceptions are found the simple
+matcher can be run.
+'''
+class Session:
+    def __init__(self):
+        self.exclusions = False
+
+'''
+This method opens the .csv file and returns a list of Participant objects from the contents.
+'''
+def importCsvFile(session):
     with open("participants.csv", 'r') as csvFile:
         csvLines = csv.reader(csvFile)
         
+        # Skip the first line, which contains titles
         next(csvLines)
         
         participants = []
 
         for line in csvLines:
+            if len(line) > 2:
+                session.exclusions = True
+            # Create Participant object for each line in the csv and add to the list
             name = line[0]
             email = line[1]
             exclude = [line[i] for i in range(2, len(line))]
             participants.append(Participant(name, email, exclude))
-            
-            # for item in line:
-            #     print(item)   
+
         
-        for item in participants:
-            print(item)
+        # for item in participants:
+        #     print(item)
+        
+        return participants
+
+'''
+Matching algorithm that does not take exclusions into account.
+Randomises the order of particpants by creating a separate list of
+'''
+def simpleMatcher(participants):
+    participantCount = len(participants)
+    givers = list(range(participantCount))
+    random.shuffle(givers)
+    receivers = givers.copy()
+    receivers.insert(0, receivers.pop())
+
+    for i in range(participantCount):
+        participants[givers[i]].match = participants[receivers[i]]
+    
+    for part in participants:
+        print(part.nameWithMatch)
+
+
+
+
+'''
+Matching algorithm that takes exclusions into account.
+'''
+def complexMatcher(participants):
+    pass
 
 
 
 
 def main():
-    importCsvFile()
-
-
+    session = Session()
+    participants = importCsvFile(session)
+    if session.exclusions:
+        complexMatcher(participants)
+    else:
+        simpleMatcher(participants)
 
 
 
--- participant.py
+++ participant.py
@@ -1,19 +1,35 @@
 class Participant:
     
-    def __init__(self, name, email = "", exclude = []):
+    def __init__(self, name, email, exclude):
         self.name = name
-        if email == "":
-            self.email = email
+        self.email = email
+        
+        if exclude:
+            self.exclude = exclude
         else:
-            self.email = "{}@email.com".format(self.name)
-        self.exclude = exclude
+            self.exclude = []
+        
+        self.match = None
     
+
+    @property
+    def nameWithMatch(self):
+        stringOut = self.name
+        if self.match:
+            stringOut += " buys for {}".format(self.match.name)
+        else:
+            stringOut += " (no match)"
+        return stringOut
+
+
 
     def __str__(self):
         returnString = "Name: {}\nemail: {}\nExclude:\n".format(self.name, self.email)
         if len(self.exclude) == 0:
             returnString += "\tNone"
-        else:
+        elif self.exclude:
             for item in self.exclude:
-                returnString += "\t" + item
+                returnString += "\t" + item + "\n"
+        else:
+            returnString += "None\n"
         return returnString
